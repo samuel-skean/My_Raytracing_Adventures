0d0c7ddc65e3ff561fd8b9abce8963ac4f54bdf0
Samuel Skean <sskean2@uic.edu>
2024-02-23 00:57:32 -0600

    First thing that involves a ray. A pretty pleasant image, actually.

--- rust/first-image/src/main.rs
+++ rust/first-image/src/main.rs
@@ -1,13 +1,37 @@
 mod vec;
+mod ray;
+
 use std::io::{stderr, Write};
-use vec::{Vec3, Color};
+
+use vec::{Vec3, Point3, Color};
+use ray::Ray;
+
+// Gets a color from each ray that forms a gradient when put together in the viewport.
+fn ray_color(r: &Ray) -> Color {
+    let unit_direction = r.direction().normalized();
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+}
 
 fn main() {
 
     // Image:
 
-    const IMAGE_WIDTH: u16 = 256;
-    const IMAGE_HEIGHT: u16 = 256;
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
+    const IMAGE_WIDTH: u64 = 256;
+    const IMAGE_HEIGHT: u64 = ((256 as f64) / ASPECT_RATIO) as u64;
+
+    // Camera
+    let viewport_height = 2.0;
+    let viewport_width = ASPECT_RATIO * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Point3::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+
+    let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0
+                                - Vec3::new(0.0, 0.0, focal_length);
 
 
     // Header:
@@ -21,9 +45,12 @@ fn main() {
         stderr().flush().unwrap();
 
         for i in 0..IMAGE_WIDTH {
-            let pixel_color = Color::new((i as f64) / ((IMAGE_WIDTH - 1) as f64),
-                                         (j as f64) / ((IMAGE_HEIGHT - 1) as f64),
-                                        0.25);
+            let u = (i as f64) / ((IMAGE_WIDTH - 1) as f64);
+            let v = (j as f64) / ((IMAGE_HEIGHT - 1) as f64);
+
+            let r = Ray::new(origin,
+                             lower_left_corner + u * horizontal + v * vertical - origin);
+            let pixel_color = ray_color(&r);
 
             print!("{} ", pixel_color.format_color());
         }
