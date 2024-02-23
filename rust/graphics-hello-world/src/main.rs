6acad0e496ab13b0e75ce7f5bcd38f02dfba2545
Samuel Skean <sskean2@uic.edu>
2024-02-22 23:05:15 -0600

    Made the hello world use floats.
    Not quite what they have in the book, they took liberties with the colors.

--- rust/graphics-hello-world/src/main.rs
+++ rust/graphics-hello-world/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
     // Image:
 
-    const IMAGE_WIDTH: u16 = 256;
-    const IMAGE_HEIGHT: u16 = 256;
+    const IMAGE_WIDTH: u64 = 256;
+    const IMAGE_HEIGHT: u64 = 256;
 
 
     // Header:
@@ -11,13 +11,16 @@ fn main() {
     println!("{IMAGE_WIDTH} {IMAGE_HEIGHT}");
     println!("255");
 
-    for j in 0..IMAGE_WIDTH {
-        for i in 0..IMAGE_HEIGHT {
-            let r = i;
-            let g = j;
+    for j in 0..IMAGE_HEIGHT {
+        for i in 0..IMAGE_WIDTH {
+            let r = (i as f64) / ((IMAGE_WIDTH - 1) as f64);
+            let g = (j as f64) / ((IMAGE_HEIGHT - 1) as f64);
             let b = 0;
 
-            print!("{r} {g} {b} ");
+            let ir = (255.999 * r) as u64;
+            let ig = (255.999 * g) as u64;
+            let ib = (255.999 * b as f64) as u64;
+            print!("{ir} {ig} {ib} ");
         }
         println!();
     }
