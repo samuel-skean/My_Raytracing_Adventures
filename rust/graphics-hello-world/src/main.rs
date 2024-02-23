d0a7417f9ab9499e442036d485d566bad2b189c7
Samuel Skean <sskean2@uic.edu>
2024-02-22 23:10:29 -0600

    Made danb's modifications
    (well, mostly, keeping the row-like structure for myself).

--- rust/graphics-hello-world/src/main.rs
+++ rust/graphics-hello-world/src/main.rs
@@ -11,15 +11,15 @@ fn main() {
     println!("{IMAGE_WIDTH} {IMAGE_HEIGHT}");
     println!("255");
 
-    for j in 0..IMAGE_HEIGHT {
+    for j in (0..IMAGE_HEIGHT).rev() {
         for i in 0..IMAGE_WIDTH {
             let r = (i as f64) / ((IMAGE_WIDTH - 1) as f64);
             let g = (j as f64) / ((IMAGE_HEIGHT - 1) as f64);
-            let b = 0;
+            let b = 0.25;
 
             let ir = (255.999 * r) as u64;
             let ig = (255.999 * g) as u64;
-            let ib = (255.999 * b as f64) as u64;
+            let ib = (255.999 * b) as u64;
             print!("{ir} {ig} {ib} ");
         }
         println!();
