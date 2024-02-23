0d458c5dabed4cb775a96651c6e9880add83dad5
Samuel Skean <sskean2@uic.edu>
2024-02-23 00:28:33 -0600

    Used Vec3 to output that same image.

--- rust/first-image/src/main.rs
+++ rust/first-image/src/main.rs
@@ -1,5 +1,33 @@
 mod vec;
+use std::io::{stderr, Write};
+use vec::{Vec3, Color};
 
 fn main() {
-    println!("Hello, world!");
+
+    // Image:
+
+    const IMAGE_WIDTH: u16 = 256;
+    const IMAGE_HEIGHT: u16 = 256;
+
+
+    // Header:
+
+    println!("P3");
+    println!("{IMAGE_WIDTH} {IMAGE_HEIGHT}");
+    println!("255");
+
+    for j in (0..IMAGE_HEIGHT).rev() {
+        eprint!("\rScanlines remaining: {:3}", j + 1);
+        stderr().flush().unwrap();
+
+        for i in 0..IMAGE_WIDTH {
+            let pixel_color = Color::new((i as f64) / ((IMAGE_WIDTH - 1) as f64),
+                                         (j as f64) / ((IMAGE_HEIGHT - 1) as f64),
+                                        0.25);
+
+            print!("{} ", pixel_color.format_color());
+        }
+        println!();
+    }
+    eprintln!("\rDone!                          ");
 }
