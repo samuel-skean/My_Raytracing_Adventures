17ee434581fef0626a5b550943e5c042fdac9158
Samuel Skean <sskean2@uic.edu>
2024-02-24 16:23:17 -0600

    My way of computing face normals.

--- rust/raytracer/src/hit.rs
+++ rust/raytracer/src/hit.rs
@@ -9,13 +9,18 @@ pub struct HitRecord {
 }
 
 impl HitRecord {
-    pub fn set_face_normal(&mut self, r: &Ray, outward_normal: Vec3) {
-        self.front_face = r.direction().dot(outward_normal) < 0.0;
-        self.normal = if self.front_face {
-            outward_normal
-        } else {
-            -1.0 * outward_normal
-        };
+    // Note: This is dodgy, I'm changing their code, but I really don't like
+    // making the HitRecord with dummy values only to overwrite them.
+    // And this way, I can have correctly initialized HitRecords without
+    // necessarily making them mutable.
+    pub fn with_normal_against_ray(p: Point3, t: f64, r: &Ray, outward_normal: Vec3) -> HitRecord {
+        let front_face = r.direction().dot(outward_normal) > 0.0;
+        HitRecord {
+            p,
+            normal: if front_face  { outward_normal } else { -1.0 * outward_normal },
+            t,
+            front_face
+        }
     }
 }
 
