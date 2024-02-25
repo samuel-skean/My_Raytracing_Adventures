04aeadf7919efcbaf8d19d905a13470940c7f05c
Samuel Skean <sskean2@uic.edu>
2024-02-25 02:18:26 -0600

    Added diffuse lighting.
    Very dark - looks darker even than the non-gamma-corrected image on the
    book online. But I've tried adding gamma correction, and it does make it
    look reasonable.

--- rust/raytracer/src/main.rs
+++ rust/raytracer/src/main.rs
@@ -20,11 +20,16 @@ use camera::Camera;
 // from light blue on the left, through white, and to light blue on the right.
 // Basically, the x stole from the y when it was pointing left and pointing
 // right. This is why the image is pretty :).
-fn ray_color(r: &Ray, world: &World) -> Color {
+fn ray_color(r: &Ray, world: &World, depth: u64, rng: &mut impl Rng) -> Color {
+    if depth == 0 {
+        return Color::new(0.0, 0.0, 0.0);
+    }
+
     if let Some(rec) = world.hit(r, 0.0, f64::INFINITY) {
-        0.5 * (rec.normal + Color::new(1.0,1.0, 1.0))
-            // Mapping the components of the normal, which have the range -1.0
-            // to 1.0 as normalized vectors, to the range 0 to 1.0
+        let target = rec.p + rec.normal + Vec3::random_in_unit_sphere(rng);
+        let r = Ray::new(rec.p, target - rec.p);
+        0.5 * ray_color(&r, world, depth - 1, rng)
+            // Sphere reflects half the light it gets.
     } else {
         let unit_direction = r.direction().normalized();
         let t = 0.5 * (unit_direction.y() + 1.0);
@@ -36,6 +41,7 @@ const ASPECT_RATIO: f64 = 16.0 / 9.0;
 const IMAGE_WIDTH: u64 = 256;
 const IMAGE_HEIGHT: u64 = ((IMAGE_WIDTH as f64) / ASPECT_RATIO) as u64;
 const SAMPLES_PER_PIXEL: u64 = 100;
+const MAX_DEPTH: u64 = 5;
 
 fn main() {
 
@@ -71,7 +77,7 @@ fn main() {
                     ((j as f64) + random_v_component) / ((IMAGE_HEIGHT - 1) as f64);
 
                 let r = cam.get_ray(u, v);
-                pixel_color += ray_color(&r, &world);
+                pixel_color += ray_color(&r, &world, MAX_DEPTH, &mut rng);
             }
 
             print!("{} ", pixel_color.format_color(SAMPLES_PER_PIXEL));
